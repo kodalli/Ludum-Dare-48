43996b6e1bab330b964b6d16c798e3d087e8c3f9
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-04-26 22:50:10 -0400

    boss logic

--- LudumDare48/Assets/Scripts/EnemyStateMachine/EnemySpecific/FinalBoss/FinalBossController.cs
+++ LudumDare48/Assets/Scripts/EnemyStateMachine/EnemySpecific/FinalBoss/FinalBossController.cs
@@ -6,13 +6,14 @@ public class FinalBossController : Singleton<FinalBossController>, IDamageable {
 
     [SerializeField] private LayerMask whatIsPlayer;
     [SerializeField] private Transform playerCheck;
-    [SerializeField] private Vector2 hitForce;
-    [SerializeField] private float maxHealth;
-    [SerializeField] private float contactDamage;
-    [SerializeField] private float playerCheckDistance;
+    [SerializeField] private Vector2 hitForce = new Vector2(4f, 5f);
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float contactDamage = 15f;
+    [SerializeField] private float playerCheckDistance = 5f;
     [SerializeField] private float drag;
-    [SerializeField] private float speed;
-    [SerializeField] private float attackTime;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float attackTime = 2f;
+    [SerializeField] private List<Transform> nextPositions = new List<Transform>();
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -21,6 +22,10 @@ public class FinalBossController : Singleton<FinalBossController>, IDamageable {
     private bool bossFightStarted;
     private bool transformationDone;
     private bool isWalking;
+    private bool hasGottenUp;
+    private bool attackDone;
+    private bool hasReachedNextPos;
+    private int index;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -30,22 +35,45 @@ public class FinalBossController : Singleton<FinalBossController>, IDamageable {
         isWalking = false;
     }
 
+#if UNITY_EDITOR
+    private void OnGUI() {
+        if (GUI.Button(new Rect(0, 0, 200, 50), "Start Fight")) {
+            StartBossFight();
+        }
+    }
+
+#endif
     private void Update() {
-        if (isWalking && !bossFightStarted) {
-            rb.velocity = Vector2.right * speed;
+        if (hasGottenUp && isWalking && !bossFightStarted) {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         } else if (isWalking && transformationDone && bossFightStarted && !CheckPlayerInRange()) {
             rb.velocity = Player.Instance.transform.position.x > transform.position.x ? Vector2.right * speed : -Vector2.right * speed;
         }
     }
 
     private void FixedUpdate() {
-        if (Player.Instance.transform.position.x > transform.position.x && !bossFightStarted) {
+
+        if (nextPositions[index].position.x > transform.position.x) {
+            hasReachedNextPos = false;
+        } else {
+            hasReachedNextPos = true;
+        }
+
+        if (Player.Instance.transform.position.x > transform.position.x && hasReachedNextPos) {
+            if (index + 1 < nextPositions.Count) {
+                index++;
+                hasReachedNextPos = false;
+            }
+        }
+
+
+        if (Player.Instance.transform.position.x > transform.position.x && !bossFightStarted && !hasReachedNextPos) {
             anim.SetBool("idle", false);
             if (!isWalking) {
                 anim.SetTrigger("getup");
                 isWalking = true;
             }
-        } else if (!bossFightStarted) {
+        } else if (!bossFightStarted && hasReachedNextPos) {
             anim.SetBool("idle", true);
         } else if (transformationDone && bossFightStarted && CheckPlayerInRange()) {
             StartAttackPhase();
@@ -60,23 +88,27 @@ public class FinalBossController : Singleton<FinalBossController>, IDamageable {
 
     public void StartBossFight() {
         bossFightStarted = true;
+        anim.SetBool("idlewalk", false);
+        isWalking = false;
         anim.SetTrigger("ultimateform");
     }
 
     public void StartAttackPhase() {
-        StartCoroutine(DoAttack());
+        anim.SetBool("bigwalk", false);
+        // StartCoroutine(DoAttack());
         // Do attack, wait for animation done, then walk
     }
-    // public void OnAttackDone() {
-    //     anim.SetBool("bigwalk", true);
-    //     isWalking = true;
-    // }
+    public void OnAttackDone() {
+        anim.SetBool("bigwalk", true);
+        isWalking = true;
+    }
     public void OnGetUpDone() {
         anim.SetBool("littlewalk", true);
         isWalking = true;
+        hasGottenUp = true;
     }
     IEnumerator DoAttack() {
-        anim.SetBool("bigwalk", false);
+
         isWalking = false;
         anim.SetTrigger("bigattack");
         var countDown = attackTime;
@@ -135,9 +167,21 @@ public class FinalBossController : Singleton<FinalBossController>, IDamageable {
 
     }
 
-    bool CheckPlayerInRange() => Physics2D.Raycast(playerCheck.position, -Vector2.right, playerCheckDistance, whatIsPlayer);
+    bool CheckPlayerInRange() {
+
+        return Physics2D.Raycast(playerCheck.position, Vector2.right, playerCheckDistance, whatIsPlayer);
+    }
 
     public void TakeDamage(float damage) {
+        Debug.Log("take damage calleed");
         throw new System.NotImplementedException();
     }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(playerCheck.position, playerCheck.position + (Vector3)(Vector2.right * playerCheckDistance));
+
+    }
+
+
 }
