a758c2e592d9c152e95c12574b56565e60e0e8a2
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-04-26 20:31:59 -0400

    boss fight

--- LudumDare48/Assets/Scripts/EnemyStateMachine/EnemySpecific/FinalBoss/FinalBossController.cs
+++ LudumDare48/Assets/Scripts/EnemyStateMachine/EnemySpecific/FinalBoss/FinalBossController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinalBossController : MonoBehaviour, IDamageable {
+public class FinalBossController : Singleton<FinalBossController>, IDamageable {
 
     [SerializeField] private LayerMask whatIsPlayer;
     [SerializeField] private Transform playerCheck;
@@ -12,26 +12,118 @@ public class FinalBossController : MonoBehaviour, IDamageable {
     [SerializeField] private float playerCheckDistance;
     [SerializeField] private float drag;
     [SerializeField] private float speed;
+    [SerializeField] private float attackTime;
 
     private Rigidbody2D rb;
     private Animator anim;
     private float currentHealth;
-    private bool isPlayerInRange;
     private bool isDetected;
+    private bool bossFightStarted;
+    private bool transformationDone;
+    private bool isWalking;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        bossFightStarted = false;
+        transformationDone = false;
+        isWalking = false;
+    }
+
+    private void Update() {
+        if (isWalking && !bossFightStarted) {
+            rb.velocity = Vector2.right * speed;
+        } else if (isWalking && transformationDone && bossFightStarted && !CheckPlayerInRange()) {
+            rb.velocity = Player.Instance.transform.position.x > transform.position.x ? Vector2.right * speed : -Vector2.right * speed;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (Player.Instance.transform.position.x > transform.position.x && !bossFightStarted) {
+            anim.SetBool("idle", false);
+            if (!isWalking) {
+                anim.SetTrigger("getup");
+                isWalking = true;
+            }
+        } else if (!bossFightStarted) {
+            anim.SetBool("idle", true);
+        } else if (transformationDone && bossFightStarted && CheckPlayerInRange()) {
+            StartAttackPhase();
+        }
     }
 
 
-    public void TakeDamage(float damage) {
-        throw new System.NotImplementedException();
+    public void MoveToNextLocation() {
+
     }
 
+
+    public void StartBossFight() {
+        bossFightStarted = true;
+        anim.SetTrigger("ultimateform");
+    }
+
+    public void StartAttackPhase() {
+        StartCoroutine(DoAttack());
+        // Do attack, wait for animation done, then walk
+    }
+    // public void OnAttackDone() {
+    //     anim.SetBool("bigwalk", true);
+    //     isWalking = true;
+    // }
+    public void OnGetUpDone() {
+        anim.SetBool("littlewalk", true);
+        isWalking = true;
+    }
+    IEnumerator DoAttack() {
+        anim.SetBool("bigwalk", false);
+        isWalking = false;
+        anim.SetTrigger("bigattack");
+        var countDown = attackTime;
+        while (countDown > 0f) {
+            countDown -= Time.deltaTime;
+            // do attack shoot something
+            yield return default;
+        }
+        isWalking = true;
+        anim.SetBool("bigwalk", true);
+    }
+
+    public void OnTransformationDone() {
+        transformationDone = true;
+        anim.SetBool("bigwalk", true);
+    }
 
     public void TakeDamage(float damage, bool hitFromRight) {
-        throw new System.NotImplementedException();
+
+
+        PlayDamageEffect();
+
+        hitForce.x *= hitFromRight ? -1 : 1;
+
+        rb.AddForce(hitForce, ForceMode2D.Impulse);
+
+        currentHealth -= (int)damage;
+
+        CinemachineShake.Instance.ShakeCamera(3f, 0.2f);
+
+        if (currentHealth <= 0) {
+            RandomDrop.SpawnRandomDrop(transform.position, Quaternion.identity);
+            Destroy(this.gameObject, 0.1f);
+        }
+        Debug.Log("Creedit God" + " " + currentHealth);
+
+        // Debug.Log(EnemyDelegateCount);
+    }
+
+    private void PlayDamageEffect() {
+        var damageEffect = ObjectPooler.Instance.SpawnFromPool("damageEffect", transform.position, Quaternion.identity);
+        var scale = transform.localScale;
+        scale.x *= -1;
+        damageEffect.transform.localScale = scale;
+        damageEffect.transform.position = transform.position;
+
+        damageEffect.GetComponent<ParticleSystem>().Play();
     }
 
 
@@ -44,4 +136,8 @@ public class FinalBossController : MonoBehaviour, IDamageable {
     }
 
     bool CheckPlayerInRange() => Physics2D.Raycast(playerCheck.position, -Vector2.right, playerCheckDistance, whatIsPlayer);
+
+    public void TakeDamage(float damage) {
+        throw new System.NotImplementedException();
+    }
 }
